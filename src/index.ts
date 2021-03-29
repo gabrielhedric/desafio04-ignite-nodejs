58a29a1b2571935928888920abe497e2d53b2228
Gabriel Hedric <gabriel.hedric@gmail.com>
2021-03-29 19:23:48 -0300

    complete project documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-/* eslint-disable prettier/prettier */
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import swaggerFile from './swagger.json';
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(express.json());
 
